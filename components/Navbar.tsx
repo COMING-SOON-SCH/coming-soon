981487cee9b2bbf44f30cef706b505886d0329d9
Antraxmin <antraxmin@naver.com>
2024-04-20 12:23:10 +0900

    update: 상단바 라우팅

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,31 @@
+import Link from "next/link";
+
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-blue-900 text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-bold">COMING SOON</div>
+        <Link href="/">
+          <div className="text-lg font-bold">COMING SOON</div>
+        </Link>
         <div className="space-x-4">
-          <a href="#" className="hover:text-gray-300">
+          <Link href="#" className="hover:text-gray-300">
             ABOUT
-          </a>
-          <a href="#" className="hover:text-gray-300">
+          </Link>
+          <Link href="/members" className="hover:text-gray-300">
             MEMBER
-          </a>
-          <a href="#" className="hover:text-gray-300">
-            TRACKS
-          </a>
-          <a href="#" className="hover:text-gray-300">
+          </Link>
+          <Link href="/track" className="hover:text-gray-300">
+            TRACK
+          </Link>
+          <Link href="/activity" className="hover:text-gray-300">
             ACTIVITY
-          </a>
-          <a href="#" className="hover:text-gray-300">
+          </Link>
+          <Link href="#" className="hover:text-gray-300">
             PROJECT
-          </a>
-          <a href="#" className="hover:text-gray-300">
+          </Link>
+          <Link href="#" className="hover:text-gray-300">
             RECRUIT
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
